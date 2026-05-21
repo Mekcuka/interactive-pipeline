2423e95c135fc233e2d098255b42e9ddf39c1364
Mekcuka <wolginws@hotmail.com>
2026-05-21 23:40:39 +0300

    fix: conditional base URL for dev/prod

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/interactive-pipeline/',
+  base: process.env.NODE_ENV === 'production' ? '/interactive-pipeline/' : '/',
 })
